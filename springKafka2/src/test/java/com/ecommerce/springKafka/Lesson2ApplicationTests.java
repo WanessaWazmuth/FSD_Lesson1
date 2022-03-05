a0b046271a2a918e47f38ff2d18ff8b203772ccf
WANESSA WAZMUTH <74206277+WanessaWazmuth@users.noreply.github.com>
2022-03-05 14:25:37 -0300

    Module2-Lesson2

---
+++ springKafka2/src/test/java/com/ecommerce/springKafka/Lesson2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.springKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
